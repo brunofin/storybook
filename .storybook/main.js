0636198a8dcb46162778052ff88060ab4ccc1d14
Bruno Finger <bruno.k.finger@gmail.com>
2022-07-18 14:37:19 +0200

    make it work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,17 @@
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../../../packages/**/*.stories.mdx",
+    "../../../packages/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
 }
